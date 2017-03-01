227e47c2f6acdf6e6ec1209f0112a5db0930aa4a
John Cobb <jgcobb3@gmail.com>
2017-03-01 10:39:13 -0500

    parse iso 8601 durations

--- forecast.py
+++ forecast.py
@@ -1,8 +1,11 @@
 import logging
 import json
+import re
+import datetime
 
 import requests
 import iso8601
+import isodate
 import pandas as pd
 
 logging = logging.getLogger(__name__)
@@ -10,7 +13,7 @@ logging = logging.getLogger(__name__)
 
 class NDFD_v3(object):
     """
-        National Digital Forecast Database
+       https://forecast-v3.weather.gov/documentation
     """
 
     def __init__(self, lat, lon):
@@ -50,10 +53,17 @@ class NDFD_v3(object):
 
     def _process_result(self, result, parameter):
         records = result['properties'][parameter]['values']
-        timeindex = [pd.Timestamp(iso8601.parse_date(vt['validTime'].split('/')[0])) for vt in records]
-        values = [v['value'] for v in records]
-        return pd.DataFrame({parameter: values}, index=timeindex)
-
+        index = []
+        values = []
+        for record in records:
+            timestamp = iso8601.parse_date(record['validTime'].split('/')[0])
+            duration = isodate.parse_duration(record['validTime'].split('/')[1])
+            while duration > datetime.timedelta(days=0, hours=0):
+                index.append(pd.Timestamp(timestamp))
+                values.append(record['value'])
+                timestamp = timestamp + datetime.timedelta(hours=1)
+                duration = duration - datetime.timedelta(hours=1)
+        return pd.DataFrame({parameter: values}, index=index)
 
     def to_dataframe(self):
 
@@ -63,6 +73,78 @@ class NDFD_v3(object):
         df1 = self._process_result(result, 'windSpeed')
         df2 = self._process_result(result, 'skyCover')
         df3 = self._process_result(result, 'relativeHumidity')
-
-        df = df0.join([df1, df2, df3], how='inner')
+        df = df0.join([df1, df2, df3])
         return df
+
+
+class CattleHeatStress(object):
+
+    def __init__(self, lat, lon):
+        self._chs = self._forecast(lat, lon)
+
+    def forecast(self):
+        return self._chs
+
+    def daily_max(self):
+        self._chs.index = pd.to_datetime(self._chs.index, utc=True)
+        return self._chs.resample('D').max()
+
+    def summary(self):
+        return [{'date': day.strftime('%m/%d/%Y'),
+                 'color': self.color_code(value),
+                 'condition': self.categorize(value)}
+                for day, value in self.daily_max().iteritems()]
+
+    def _forecast(self, lat, lon):
+
+        df = NDFD_v3(lat, lon).to_dataframe()
+        return df.apply(lambda row: self.calculate(row.temperature,
+                                                   row.relativeHumidity,
+                                                   row.windSpeed,
+                                                   row.skyCover), axis=1)
+
+    @staticmethod
+    def calculate(temperature, relative_humidity, wind_speed, cloud_amount):
+
+        # Approximate solar radiation.
+        solar = 1110 - (8.9 * cloud_amount)
+
+        # Convert units
+        temperature = temperature * 1.8 + 32
+        wind_speed = wind_speed * 2.23694
+
+        # Calculate cattle heat stress, see:
+        # http://www.ars.usda.gov/Main/docs.htm?docid=15616
+        breathing_rate = (2.83 * temperature) + (0.58 * relative_humidity) - (0.76 * wind_speed) + (0.039 * solar) - 196.4
+
+        # Set minimum and maximum values.
+        breathing_rate = 10 if breathing_rate < 10 else breathing_rate
+        breathing_rate = 150 if breathing_rate > 150 else breathing_rate
+
+        return breathing_rate
+
+    @staticmethod
+    def categorize(breathing_rate):
+
+        if breathing_rate < 90:
+            return 'NORMAL'
+        elif 90 <= breathing_rate < 110:
+            return 'ALERT'
+        elif 110 <= breathing_rate < 130:
+            return 'DANGER'
+        elif 130 <= breathing_rate:
+            return 'EMERGENCY'
+
+    @staticmethod
+    def color_code(breathing_rate):
+
+        color = '#FFFFFF'
+        if breathing_rate < 90:
+            color = '#0099ff'
+        elif 90 <= breathing_rate < 110:
+            color = '#ffff00'
+        elif 110 <= breathing_rate < 130:
+            color = '#ff9933'
+        elif 130 <= breathing_rate:
+            color = '#ff0000'
+        return color
