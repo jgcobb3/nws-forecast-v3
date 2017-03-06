347ffdb57707e92124729ddd1d5cc25714298e78
John Cobb <jgcobb3@gmail.com>
2017-03-06 13:54:40 -0500

    use isodate library to build index

--- nws_forecast_v3.py
+++ nws_forecast_v3.py
@@ -3,7 +3,6 @@ import json
 import datetime
 
 import requests
-import iso8601
 import isodate
 import pandas as pd
 
@@ -15,30 +14,24 @@ class GridPoint(object):
        https://forecast-v3.weather.gov/documentation
     """
 
-    def __init__(self, lat, lon, user='serchlights@fs.fed.us'):
+    def __init__(self, lat, lon, user='https://github.com/jgcobb3/nws-forecast-v3'):
 
         self.lat = lat
         self.lon = lon
-        self.headers = {'accept': 'application/geo+json',
-                        'user-agent': user}
+        self.user = user
 
-    def _get_grid_url(self):
+    def _grid_url(self):
 
         url = 'https://api.weather.gov/points/{},{}'.format(self.lat, self.lon)
-        try:
-            response = requests.get(url, headers=self.headers)
-            response.raise_for_status()
-            logging.info(response.url)
-            result = json.loads(response.content)
-            return result['properties']['forecastGridData']
-        except requests.exceptions.RequestException, e:
-            logging.error(e)
-            raise e
+        result = self._get(url)
+        return result['properties']['forecastGridData']
 
-    def _get_grid_data(self, url):
+    def _get(self, url):
 
         try:
-            response = requests.get(url, headers=self.headers)
+            headers = {'accept': 'application/geo+json',
+                       'user-agent': self.user}
+            response = requests.get(url, headers=headers)
             response.raise_for_status()
             logging.info(response.url)
             result = json.loads(response.content)
@@ -47,32 +40,34 @@ class GridPoint(object):
             logging.error(e)
             raise e
 
-    def _process_result(self, result, property):
-        records = result['properties'][property]['values']
-        index = []
-        values = []
-        for record in records:
-            timestamp = iso8601.parse_date(record['validTime'].split('/')[0])
-            duration = isodate.parse_duration(record['validTime'].split('/')[1])
-            while duration > datetime.timedelta(days=0, hours=0):
-                index.append(pd.Timestamp(timestamp))
-                values.append(record['value'])
-                timestamp = timestamp + datetime.timedelta(hours=1)
-                duration = duration - datetime.timedelta(hours=1)
-        return pd.DataFrame({property: values}, index=index)
+    def _read_property(self, result, property):
 
+        df = pd.DataFrame(columns=[property])
+        records = result['properties'][property]['values']
+        for record in records:
+            index = self._build_index(record['validTime'])
+            values = [record['value']] * len(index)
+            df = df.append(pd.DataFrame(values, index=index, columns=[property]))
+        return df
+
+    def _build_index(self, validTime):
+
+        index = []
+        timestamp = isodate.parse_datetime(validTime.split('/')[0])
+        duration = isodate.parse_duration(validTime.split('/')[1])
+        while duration > datetime.timedelta(days=0, hours=0):
+            index.append(pd.Timestamp(timestamp))
+            timestamp = timestamp + datetime.timedelta(hours=1)
+            duration = duration - datetime.timedelta(hours=1)
+        return pd.to_datetime(index, utc=True)
 
     def to_dataframe(self):
 
-        df = pd.DataFrame()
-        result = self._get_grid_data(self._get_grid_url())
+        result = self._get(self._grid_url())
+        df = pd.DataFrame(index=self._build_index(result['properties']['validTimes']))
         meta = ['@id', '@type', 'updateTime', 'validTimes', 'elevation',
-                'forecastOffice', 'gridId', 'gridX', 'gridY']
+                'forecastOffice', 'gridId', 'gridX', 'gridY', 'weather']
         for property in result['properties'].keys():
             if property not in meta:
-                if df.empty:
-                    df = self._process_result(result, property)
-                else:
-                    df[property] = self._process_result(result, property)
-        df.index = pd.to_datetime(df.index, utc=True)
+                df[property] = self._read_property(result, property)
         return df
